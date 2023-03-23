3f0acc3efd45fe85dd769bd2f4af08262e12cfd1
Jakub Nowak <jakub.nowak@zst.kolbuszowa.pl>
2023-03-23 12:19:21 +0100

    bool

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // napisz program prawda fałsz
+using namespace std;
+int main() {
+bool czyprogramowaniejestciekawe = true;
+bool czylubiechodzicdoszkoly = false;
+cout << czyprogramowaniejestciekawe << "\n";
+cout << czylubiechodzicdoszkoly << "\n";
+    return 0;
+}
